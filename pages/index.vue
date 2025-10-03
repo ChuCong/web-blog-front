9f258936f2756b7732033bf21da5e036e3e39754
Chu Cong <cong01666306869@gmail.com>
2025-10-03 19:40:39 +0700

    fix bug

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <img v-if="articleComp.state.articles[0]?.url" :src="articleComp.state.articles[0].url?.toString()"
             class="md:w-[350px] rounded-[10px]" alt="">
           <a :href="`/bai-viet/${articleComp.state.articles[0]?.slug}`">
-            <div class="text-[#353637] md:py-0 py-3 text-[20px] font-semibold hover:text-[#1894D7] h-[59px] overflow-hidden">
+            <div class="text-[#353637] md:py-0 py-3 text-[20px] font-semibold hover:text-[#F36F21] h-[59px] overflow-hidden">
               {{ articleComp.state.articles[0]?.title }}
             </div>
             <div class="flex items-center gap-2 my-4">
@@ -24,7 +24,7 @@
       <div class="w-[30%] desktop border-l-2 pl-5 h-fit">
         <div class="flex md:flex-wrap gap-3">
           <div v-for="value in categoryComp.state.categories" :key="value.id"
-            class="border border-gray-200 rounded-full px-4 py-2 text-sm text-center hover:bg-[#1894D7] hover:text-[#fff] ">
+            class="border border-gray-200 rounded-full px-4 py-2 text-sm text-center hover:bg-[#F36F21] hover:text-[#fff] ">
             <a :href="`/chuyen-muc/${value.slug}`" class="cursor-pointer">{{ value.title }}</a>
           </div>
         </div>
@@ -34,7 +34,7 @@
       <div v-for="value in articleComp.state.articles.slice(2)" :key="value.id">
         <a :href="`/bai-viet/${value.slug}`">
           <img v-if="value.url" :src="value.url?.toString()" class=" rounded-[10px]" alt="">
-          <div class="text-[#353637] py-3 text-[16px] font-semibold hover:text-[#1894D7] h-[59px] overflow-hidden">
+          <div class="text-[#353637] py-3 text-[16px] font-semibold hover:text-[#F36F21] h-[59px] overflow-hidden">
             {{ value.title }}
           </div>
           <div class="flex items-center gap-2 my-4">
